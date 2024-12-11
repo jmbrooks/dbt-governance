4ca820f49c2f1be5c64b6f6ef7e5efc308f6392d
Johnathan Brooks <19945186+jmbrooks@users.noreply.github.com>
2024-12-11 01:30:27 -0700

    Constants expansion for base behavior, log dir to log config

--- dbt_governance/constants.py
+++ dbt_governance/constants.py
@@ -1,7 +1,10 @@
 import os
 
+PROJECT_NAME = "dbt-governance"
+
 DEFAULT_CONFIG_PATH = os.path.expanduser("~/.dbt-governance/config.yml")
+DEFAULT_OUTPUT_FILE_NAME = "governance-results.json"
+DEFAULT_RULES_FILE_NAME = "governance-rules.yml"
 
-
-LOG_DIR = os.path.expanduser("~/dbt-governance-logs")
-
+# dbt project rule defaults
+DEFAULT_OWNER_META_PROPERTY_NAME = "owner"
--- dbt_governance/logging_config.py
+++ dbt_governance/logging_config.py
@@ -1,15 +1,40 @@
 import logging
-from logging.handlers import RotatingFileHandler
 import os
+import sys
+from dataclasses import dataclass
+from enum import Enum
+from logging.handlers import RotatingFileHandler
 
-from dbt_governance.constants import LOG_DIR
-
-# Base log directory
-os.makedirs(LOG_DIR, exist_ok=True)  # Create the directory if it doesn't exist
+import colorama
 
 # Log file paths
+LOG_DIR = os.path.expanduser("~/dbt-governance-logs")
 DEBUG_LOG_FILE = os.path.join(LOG_DIR, "debug.log")
 ERROR_LOG_FILE = os.path.join(LOG_DIR, "error.log")
+
+if sys.platform == "win32" and (not os.getenv("TERM") or os.getenv("TERM") == "None"):
+    colorama.init(wrap=True)
+
+
+@dataclass
+class Color(Enum):
+    """Enumeration of color codes for console output.
+
+    Attributes:
+        RED: ANSI color code for red text.
+        GREEN: ANSI color code for green text.
+        YELLOW: ANSI color code for yellow text.
+        RESET_ALL: ANSI color code to reset all text formatting
+    """
+
+    RED = colorama.Fore.RED
+    GREEN = colorama.Fore.GREEN
+    YELLOW = colorama.Fore.YELLOW
+    RESET_ALL = colorama.Style.RESET_ALL
+
+
+# Base log directory
+os.makedirs(LOG_DIR, exist_ok=True)
 
 # Logging format
 LOG_FORMAT = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
@@ -37,3 +62,26 @@ logging.basicConfig(
 
 # Create a logger for the package
 logger = logging.getLogger("dbt_governance")
+
+
+def return_in_color(text: str, color_code: Color, use_color: bool = True) -> str:
+    """Output the given text in the specified color, so long as `use_color` is set to True (the default)."""
+    if use_color:
+        return "{}{}{}".format(color_code.value, text, Color.RESET_ALL.value)
+    else:
+        return text
+
+
+def green(text: str) -> str:
+    """Output given text in green."""
+    return return_in_color(text, Color.GREEN)
+
+
+def yellow(text: str) -> str:
+    """Output given text in yellow."""
+    return return_in_color(text, Color.YELLOW)
+
+
+def red(text: str) -> str:
+    """Output given text in red."""
+    return return_in_color(text, Color.RED)
